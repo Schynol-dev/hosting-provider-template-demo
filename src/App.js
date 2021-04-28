9a04e355acee4e5c07f159787d23366c41fed8ff
Schynol-dev <kamil.schynol0dev@gmail.com>
2021-04-28 19:43:28 +0200

    Reusable navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from "react-router-dom";
 
 import './assets/fonts/fonts.css';
 import './assets/css/main.css';
+import arrow from './assets/icons/arrow-down.svg';
+import cart from './assets/icons/cart.svg';
 
 import Navigation from './components/Navigation';
 import Hosting from './components/Hosting';
@@ -13,7 +16,23 @@ import Hosting from './components/Hosting';
 function App() {
   return (
     <Router>
-      <Navigation />
+      <Navigation 
+        leftContent={<h4 className="bold-black">LiveHost</h4>}
+        rightContent={
+          <div className="flex-container">
+            <ul className="nav">
+                <li className="nav__item"><Link className="nav__link">Website Builder</Link></li>
+                <li className="nav__item"><Link className="nav__link">Hosting</Link><img className="nav__dropdown" src={arrow} /></li>
+                <li className="nav__item"><Link className="nav__link">Domain</Link><img className="nav__dropdown" src={arrow} /></li>
+                <li className="nav__item"><Link className="nav__link">Email</Link><img className="nav__dropdown" src={arrow} /></li>
+            </ul>
+            <ul className="nav">
+                <li className="nav__item"><img className="nav__cart" src={cart} /></li>
+                <li className="nav__item"><Link className="nav__link bold">Login</Link></li>
+            </ul>
+          </div>
+        }
+      />
 
       <Switch>
         <Route exact path="/">
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,26 +1,12 @@
-import { Link } from "react-router-dom";
-
-import arrow from '../assets/icons/arrow-down.svg';
-import cart from '../assets/icons/cart.svg';
-
-function Navigation() {
+function Navigation(props) {
 
     return (
         <div className="menu">
-            <div className="menu__brand">
-                <h4 className="menu__logo bold-black">LiveHost</h4>
+            <div className="menu__left-side">
+                {props.leftContent}
             </div>
-            <div className="menu__links">
-                <ul className="nav">
-                    <li className="nav__item"><Link className="nav__link">Website Builder</Link></li>
-                    <li className="nav__item"><Link className="nav__link">Hosting</Link><img className="nav__dropdown" src={arrow} /></li>
-                    <li className="nav__item"><Link className="nav__link">Domain</Link><img className="nav__dropdown" src={arrow} /></li>
-                    <li className="nav__item"><Link className="nav__link">Email</Link><img className="nav__dropdown" src={arrow} /></li>
-                </ul>
-                <ul className="nav">
-                    <li className="nav__item"><img className="nav__cart" src={cart} /></li>
-                    <li className="nav__item"><Link className="nav__link bold">Login</Link></li>
-                </ul>
+            <div className="menu__right-side">
+                {props.rightContent}
             </div>    
         </div>
     );
