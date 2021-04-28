fbcfab68d3ec8260f3d95a2d5845718763afb5be
Schynol-dev <kamil.schynol0dev@gmail.com>
2021-04-28 14:10:21 +0200

    typography setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './assets/css/main.css';
 function App() {
   return (
     <div className="App">
+      <h1>Header1</h1>
+      <h2>Header2</h2>
+      <h3>Header3</h3>
+      <h4>Header4</h4>
+      <h5>Header5</h5>
+      <h6>Header6</h6>
+      <p>Normal text</p>
+      <a>Link</a>
     </div>
   );
 }
